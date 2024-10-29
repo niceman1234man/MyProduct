0b6c309290f5b75ca13622afe7cbdf34e093f40c
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-29 13:19:30 -0700

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import{FaShoppingCart,FaUser} from 'react-icons/fa'
 
 function Header() {
   return (
-    <div>
-      <div className='flex'>
+    <div className='  py-6 bg-slate-500'>
+      <div className='flex justify-between max-w-[1240px]  mx-auto'>
       <h1>MyShop</h1>
         <div className='flex mx-auto'>
         <p className='flex justify-center items-center mx-5'><FaShoppingCart/>cart</p>
