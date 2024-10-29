84dfc0f128f06327159d9d6d447ea44bcd21c63c
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-29 13:18:41 -0700

    Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
+  const year=new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-slate-500">
+      <div  className="max-w-[1240px] mx-auto text-center">
+      <p>MyShop &copy; {year}</p>
+      </div>
+    
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
