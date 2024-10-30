643ca1b872f6ac0c6f28b7991ae52956a3fc2a75
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-29 23:33:59 -0700

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-
+import HomeScreen from './screens/HomeScreen.jsx'
+import { Route,Routes } from 'react-router-dom'
 function App() {
   return (
-    <>
-<Header/>
-<p className='text-blue-700'>hello</p>
-
-
-
-<Footer/>
-
-    </>
-  )
+    <Routes>
+      <Route 
+        path='/' 
+        element={
+          <>
+            <Header />
+            <HomeScreen />
+            <Footer />
+          </>
+        }
+      />
+      <Route path='/product/:id' element={<ProductScreen />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
