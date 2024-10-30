a771f4f96601b02df8dbfff8c9888ab70ff5849f
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-29 23:30:18 -0700

    products

---
+++ frontend/src/Product.js
@@ -0,0 +1,46 @@
+const product=[
+    {
+    _id:1,
+    name:"phone",
+    price:99,
+    image:'images/cameraman.jpg',
+    description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    },
+    {
+        _id:2,
+        name:"phone1",
+        price:99,
+        image:'images/cameraman.jpg',
+        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    },
+    {
+        _id:3,
+        name:"phone2",
+        price:99,
+        image:'images/cameraman.jpg',
+        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    },
+    {
+        _id:4,
+        name:"phone3",
+        price:99,
+        image:'images/cameraman.jpg',
+        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    },
+    {
+        _id:5,
+        name:"phone4",
+        price:99,
+        image:'images/cameraman.jpg',
+        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    },
+    {
+        _id:6,
+        name:"phone5",
+        price:99,
+        image:'images/cameraman.jpg',
+        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    }
+]
+
+export default product
