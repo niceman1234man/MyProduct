50344849beb42e478f10eb340b113231f1977c2c
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-30 00:29:34 -0700

    product

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Product() {
+  return (
+    <div className='mx-auto max-w-[1240px] py-8'>
+        <div className='text-center '>
+       <h1 className='text-xl py-3'>Latest Products</h1>
+
+        </div>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+        {
+          
+        product.map((product)=>(
+          <div className='flex flex-col items-center justify-center py-3 shadow-xl'>
+           
+            <Link to={`/product/${product._id}`}>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            </Link>
+            
+            <p>${product.price}</p>
+          </div>
+          
+        ))
+        }
+        </div>
+        <div>
+         
+
+        </div>
+        </div>
+  )
+}
+
+export default Product
