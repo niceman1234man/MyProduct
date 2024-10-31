21f9dce886c0e89fac5e489c02b21ee9ca15eb16
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-30 20:42:20 -0700

    product

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -4,42 +4,55 @@ const product=[
     name:"phone",
     price:99,
     image:'images/cameraman.jpg',
-    description:"promax phone iphone sumsung you want if yo want buy it please ee"
+    description:"promax phone iphone sumsung you want if yo want buy it please ee",
+    rate:4,
+    numberView:8
     },
     {
         _id:2,
         name:"phone1",
         price:99,
         image:'images/cameraman.jpg',
-        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+        description:"promax phone iphone sumsung you want if yo want buy it please ee",
+        rate:3.5,
+        numberView:5
     },
     {
         _id:3,
         name:"phone2",
         price:99,
         image:'images/cameraman.jpg',
-        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+        description:"promax phone iphone sumsung you want if yo want buy it please ee",
+        rate:4.5,
+        numberView:20
     },
     {
         _id:4,
         name:"phone3",
         price:99,
         image:'images/cameraman.jpg',
-        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+        description:"promax phone iphone sumsung you want if yo want buy it please ee",
+        rate:1.5,
+        numberView:3
     },
     {
         _id:5,
         name:"phone4",
         price:99,
         image:'images/cameraman.jpg',
-        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+        description:"promax phone iphone sumsung you want if yo want buy it please ee",
+        rate:4.5,
+        numberView:20
     },
     {
         _id:6,
         name:"phone5",
         price:99,
         image:'images/cameraman.jpg',
-        description:"promax phone iphone sumsung you want if yo want buy it please ee"
+        description:"promax phone iphone sumsung you want if yo want buy it please ee",
+        rate:5,
+        numberView:30
+        
     }
 ]
 
