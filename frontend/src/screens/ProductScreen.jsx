e2cdd033beb3430a0c0cb3bf3e198233c756dec0
niceman1234man <yihunietarekegn18@gmail.com>
2024-10-30 20:44:00 -0700

    product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -5,22 +5,31 @@ import Products from '../Product'; // Ensure Products is an array of product obj
 function ProductScreen() {
     const navigate = useNavigate();
     const { id: productId } = useParams();
-    console.log('Product ID:', productId); // Debugging line
-  const pro= Products.find((p)=>p._id==productId);
-    console.log("Pro",pro)// Ensure comparison is correct
-     
+  const pro= Products.find((p)=>p._id==productId);  
     return (
-        <div style={{ padding: '20px' }}>
-            <Link onClick={() => navigate(-1)}>Go Back</Link>
+        <div className='max-w-[1240px] mx-auto flex-col my-3' >
+            <Link onClick={() => navigate(-1)} className=' bg-cyan-400 mx-auto text-center mb-5 p-2'>Go Back</Link>
             {pro ? (
-                <div>
+                <div className='flex '>
+                    <div className='bg-gray-600 p-2 mt-4'>
                     <img 
                         src={pro.image} 
                         alt={pro.name} 
                        
                     />
+                    </div>
+                    <div className='ml-5'>
                     <h2>{pro.name}</h2>
+                    <p>${pro.price}</p>
                     <p>{pro.description}</p>
+                    </div>
+                    
+                   <div  className='shadow-2xl p-8 mx-auto'>
+                   <p className='p-2'>Price: ${pro.price}</p>
+                   <p className='p-2'>Status: {pro.status}</p>
+                   <button  className='p-2 bg-green-400'>Add to cart</button>
+
+                   </div>
                 </div>
             ) : (
                 <p>No item exists</p>
@@ -29,4 +38,4 @@ function ProductScreen() {
     );
 }
 
-export default ProductScreen;
+export default ProductScreen; 
