f41f5d375a42ebdcc3d48c82baa4452b87903618
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-01 14:26:00 -0700

    server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config();
 const app =express();
-app.get('/',(req,res)=>{
-  res.send("hello world");
+const port=process.env.PORT ||5000;
+app.use(cors());
+app.get('/api/product',(req,res)=>{
+  res.send("hello world    dgfcghjgkhl;");
 })
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("server running");
 })
