9119d982584e3c29e0d64f0c9a9a943aaf082821
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-02 08:41:18 -0700

    user

---
+++ backend/data/user.js
@@ -0,0 +1,20 @@
+const User = [
+  {
+    name: "admin",
+    email: "admin@gmail.com",
+    password: "12345",
+    isAdmin: true,
+  },
+  {
+    name: "user1",
+    email: "user1@gmail.com",
+    password: "12345",
+    isAdmin: false,
+  },
+  {
+    name: "user2",
+    email: "user2@gmail.com",
+    password: "12345",
+    isAdmin: false,
+  },
+];
