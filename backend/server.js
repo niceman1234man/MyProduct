ecdd32d81f43e6c2047da09985bb326859360f90
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-01 20:59:52 -0700

    server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import products from './data/Product.js';
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config();
 const app =express();
 const port=process.env.PORT ||5000;
+app.use(express.json());
 app.use(cors());
 app.get('/api/product',(req,res)=>{
-  res.send("hello world    dgfcghjgkhl;");
+ res.json(products);
 })
 
 app.listen(port,()=>{
