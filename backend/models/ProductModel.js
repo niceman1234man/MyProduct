732ddc7c1e771abe4db0e4d46988aea1fb2f3300
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-03 20:46:31 -0800

    models

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+const reviewSchema=mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User",
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+
+    },
+    comment:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true});
+
+
+
+const ProductSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User",
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+
+    },
+    brand:{
+        type:String,
+        required:true,
+    },
+    catagory:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    reviews:[reviewSchema],
+    rating:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    numReviws:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    price:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    countInStock:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    
+},{timestamps:true})
+
+
+const Product =mongoose.model("Product",ProductSchema);
+export default Product;
